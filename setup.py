b5e99317f1f6d1e663484105125ea5adb22af164
JaremGallegos <139504059+JaremGallegos@users.noreply.github.com>
2025-03-27 13:02:31 -0500

    CONFIG

--- setup.py
+++ setup.py
@@ -14,5 +14,21 @@ setup(
     url = "https://github.com/JaremGallegos/gestionar-clientes-python.git",
     author = "JaremGallegos",
     author_email = "73142526@continental.edu.pe",
-    license = "MIT"
+    license = "MIT",
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.12.4",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        "customtkinter >= 5.2.2",
+        "bson >= 0.5.10",
+    ],
+    extras_requieres = {
+        "dev": [
+            "pillow >= 11.0.0",
+            "setuptools >= 75.8.2"
+        ],
+    },
+    python_requires = ">= 3.12.4"
 )
