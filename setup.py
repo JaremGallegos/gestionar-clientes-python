43604a7ef7931d81e37689a570434d3dc19813bf
JaremGallegos <139504059+JaremGallegos@users.noreply.github.com>
2025-03-27 12:51:07 -0500

    Initial Commit Project

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+    
+setup(
+    name = "gestionar-clientes",
+    version = "0.1.0",
+    description = "Un proyecto para poder gestionar clientes de una empresa eCommerce",
+    package_dir = {"": "src"},
+    packages = find_packages(where = "src"),
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = ""
+)
