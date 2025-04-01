2328281e6fb0dd30ecefecbb4fb7af4db0276b41
JaremGallegos <139504059+JaremGallegos@users.noreply.github.com>
2025-04-01 13:08:20 -0500

    gestor-clientes version 0.1.12

--- src/view/panels/panel_categoria.py
+++ src/view/panels/panel_categoria.py
@@ -3,4 +3,10 @@ import customtkinter as ctk
 
 class PanelCategoriaLaboralDesign:
     def __init__(self, panel_principal):
-        pass
+        self._barra_superior = ctk.CTkFrame(panel_principal)
+        self._barra_superior.pack(side = ctk.TOP, fill = 'both', expand = False)
+        self._barra_inferior = ctk.CTkFrame(panel_principal)
+        self._barra_inferior.pack(side = ctk.BOTTOM, fill = 'both', expand = True)
+        self._label_titulo = ctk.CTkLabel(self._barra_superior, text = "Página Categoría Laboral", height = 20)
+        self._label_titulo.configure(fg_color = "#222d33", font = ('Roboto', 30), bg_color = COLOR_CUERPO_PRINCIPAL)
+        self._label_titulo.pack(side = ctk.TOP, fill = 'both', expand = True)
